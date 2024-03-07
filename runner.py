7deab8218f58ac9d9141c096929d36cc73ddfacd
Mike Warfe <warfejm@nih.gov>
2024-03-07 15:45:44 -0500

    updates

--- runner.py
+++ runner.py
@@ -1,16 +1,17 @@
 from prefect import flow, task, get_run_logger
 import boto3
 import sys
+import json
 
 @task
-def list():
+def list(bucket):
     s3_client = boto3.client('s3')
     paginator = s3_client.get_paginator('list_objects_v2')
     for page in paginator.paginate(Bucket=bucket):
-        contents = page['Contents']
-        logger.info(contents)
-        sys.export()    
-    
+        obj = page['Contents']
+        return(json.dumps(obj, indent=2 , default=str))
+        sys.exit()
+        
 
 @flow(name="CDS Log Analysis")
 def runner(bucket):
