04d3a7ccfaecc0fa729c9f73c0fc516dc6083eac
Mike Warfe <warfejm@nih.gov>
2024-03-07 15:52:36 -0500

    updates

--- runner.py
+++ runner.py
@@ -6,11 +6,10 @@ import json
 @task
 def list(bucket):
     s3_client = boto3.client('s3')
-    paginator = s3_client.get_paginator('list_objects_v2')
-    for page in paginator.paginate(Bucket=bucket):
-        obj = page['Contents']
-        return(json.dumps(obj, indent=2 , default=str))
-        sys.exit()
+    # Get a list of buckets
+    buckets = s3_client.list_buckets()['Buckets']
+    return(json.dumps(buckets, indent=2 , default=str))
+    sys.exit()
         
 
 @flow(name="CDS Log Analysis")
